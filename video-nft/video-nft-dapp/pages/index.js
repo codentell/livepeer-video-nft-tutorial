f82bcbb9a1a12c7b03a5d947cc4d539dcc49bc16
drewhoang <drewhoang@drewhoangs-MacBook-Pro.local>
2022-03-30 15:01:50 -0700

    remove node_modules and next cache

--- video-nft/video-nft-dapp/pages/index.js
+++ video-nft/video-nft-dapp/pages/index.js
@@ -1,6 +1,13 @@
 
 import React, { useState } from "react";
 
+
+const Video = ({ video }) => {
+  return <video width='500' height='500' controls>
+    <source src={video}></source>
+  </video>
+}
+
 export default function Home() {
   const [loading, setLoading] = useState(false);
   const [selectedFile, setSelectedFile] = useState("");
@@ -17,24 +24,20 @@ export default function Home() {
     setLoading(true)
     const response = await fetch('http://localhost:3001/upload', { method: "POST", body: formData, mode: "cors" });
     const data = await response.json();
-    setLoading(false)
+
 
     console.log(data);
     const ipfs = await fetch(`https://ipfs.io/${data.data.replace(":", "")}`);
     const nftMetadata = await ipfs.json()
     console.log(nftMetadata);
     setVideo(`https://ipfs.io/${nftMetadata.properties.video.replace(":", "")}`)
-    
+    setLoading(false)
   }
 
   return (
     <div>
-      {loading ? "Loading..." : <div>
-        <video width='320' height='320' controls>
-        <source src={video}></source>
-        </video>
-        </div>
-        }
+      {loading ? "Loading..." : <Video video={video}/>
+      }
       <input
         type="file"
         onChange={(e) => setSelectedFile(e.target.files[0])}
@@ -42,6 +45,7 @@ export default function Home() {
       <button onClick={videoUpload}>
         Upload!
       </button>
+      
     </div>
   )
 }
